e17e6a0ab1072465b2326811ecdbaee12723367b
LeoKang <xeungjin@nate.com>
2024-07-12 10:26:39 +0900

    Q.150

--- src/q150/Main.java
+++ src/q150/Main.java
@@ -1,7 +1,18 @@
 package q150;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("intellij test");
+        Scanner sc = new Scanner(System.in);
+
+        char[] ar = new char[10];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.next().charAt(0);
+        }
+
+        for (int i = ar.length - 1; i >= 0; i--) {
+            System.out.print(ar[i] + " ");
+        }
     }
 }
